f4407a4e0875c305100d3ef6e5b106c061eb9988
Lukasz Filipowski <l.filipowski@clickmaster.pl>
2017-11-13 14:51:02 +0100

    getWeatheForCity function guzzlehttp required yangqi/htmldom required

--- app/City.php
+++ app/City.php
@@ -3,6 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use GuzzleHttp\Client;
+use App\Weather;
+use App\WeatherParam;
 
 class City extends Model
 {
@@ -13,8 +16,44 @@ class City extends Model
     protected $table = 'cities';
     protected $primaryKey = 'id';
     
-    public function weathers() {
+    public function weathers() 
+    {
         
         return $this->hasMany('App\Weather');
+    }
+    
+    public function getWeatherForCity() 
+    {
+        $client = new Client();
+        $res = $client->get('https://pogoda.onet.pl/ajax/search?query='.$this->name);
+
+        if ($res->getStatusCode() == 200) {
+            $result = json_decode($res->getBody());
+  
+            if (!isset($result->suggestions[0])) {
+                
+                return false;
+            }
+            
+            $url = $result->suggestions[0]->data->url;
+            $html = new \Htmldom('https://pogoda.onet.pl'.$url);
+            $weatherBox = $html->find('div.mainBox')[0];
+            
+            $w = new Weather;
+            $w->temp = $weatherBox->find('div.temp')[0]->plaintext;
+            $w->feel_temp = $weatherBox->find('span[class="feelTempValue"]')[0]->plaintext;
+            $w->forecast = $weatherBox->find('span.iconHolder img')[0].' '.$weatherBox->find('div.forecastDesc')[0]->plaintext;
+            
+            $this->weathers()->save($w);
+            
+            foreach ($weatherBox->find('div.weatherParams li') as $element) {
+                $param = new WeatherParam;
+                $param->label = $element->find('.restParamLabel')[0]->plaintext;
+                $param->value = $element->find('.restParamValue')[0]->plaintext;
+                $w->params()->save($param);
+            }
+        }
+        
+        return true;
     }
 }
