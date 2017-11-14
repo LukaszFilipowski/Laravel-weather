7ffd9709adfc96d1672c178e8ebb0d0ce8e0e3fe
Lukasz Filipowski <lukasz.filipowski19@gmail.com>
2017-11-14 12:11:26 +0100

    update city.php
    pull request fix

--- app/City.php
+++ app/City.php
@@ -68,39 +68,4 @@ class City extends Model
         
         return true;
     }
-    
-    public function getWeatherForCity() 
-    {
-        $client = new Client();
-        $res = $client->get('https://pogoda.onet.pl/ajax/search?query='.$this->name);
-
-        if ($res->getStatusCode() == 200) {
-            $result = json_decode($res->getBody());
-  
-            if (!isset($result->suggestions[0])) {
-                
-                return false;
-            }
-            
-            $url = $result->suggestions[0]->data->url;
-            $html = new \Htmldom('https://pogoda.onet.pl'.$url);
-            $weatherBox = $html->find('div.mainBox')[0];
-            
-            $w = new Weather;
-            $w->temp = $weatherBox->find('div.temp')[0]->plaintext;
-            $w->feel_temp = $weatherBox->find('span[class="feelTempValue"]')[0]->plaintext;
-            $w->forecast = $weatherBox->find('span.iconHolder img')[0].' '.$weatherBox->find('div.forecastDesc')[0]->plaintext;
-            
-            $this->weathers()->save($w);
-            
-            foreach ($weatherBox->find('div.weatherParams li') as $element) {
-                $param = new WeatherParam;
-                $param->label = $element->find('.restParamLabel')[0]->plaintext;
-                $param->value = $element->find('.restParamValue')[0]->plaintext;
-                $w->params()->save($param);
-            }
-        }
-        
-        return true;
-    }
 }
